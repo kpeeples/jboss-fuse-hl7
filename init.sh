3b09bce15188ef03bf8e1384e4e787c13d8506ca
Kenneth Peeples <kennethwpeeples@gmail.com>
2013-05-15 20:15:55 -0400

    dd project

---
+++ init.sh
@@ -0,0 +1,80 @@
+#!/bin/sh 
+DEMO="JBoss Fuse EIP Demo"
+VERSION=6.0.0
+FUSE=jboss-fuse-6.0
+FUSE_BIN=jboss-fuse-full-6.0.0.redhat-024.zip
+DEMO_HOME=./target
+FUSE_HOME=$DEMO_HOME/$FUSE
+SERVER_CONF=$FUSE_HOME/etc
+SRC_DIR=./installs
+PRJ_DIR=./projects/fuse-eip-quickstart
+
+
+echo
+echo "Setting up the Red Hat ${DEMO} environment..."
+echo
+
+# double check for maven.
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [[ -r $SRC_DIR/$FUSE_BIN || -L $SRC_DIR/$FUSE_BIN ]]; then
+		echo $DEMO sources are present...
+		echo
+else
+		echo Need to download $FUSE_BIN package from the Customer Support Portal 
+		echo or jboss.org and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+
+# Create the target directory if it does not already exist.
+if [ ! -x $DEMO_HOME ]; then
+		echo "  - creating the demo home directory..."
+		echo
+		mkdir $DEMO_HOME
+else
+		echo "  - detected demo home directory, moving on..."
+		echo
+fi
+
+
+# Move the old JBoss instance, if it exists, to the OLD position.
+if [ -x $FUSE_HOME ]; then
+		echo "  - existing JBoss Fuse detected..."
+		echo
+		echo "  - moving existing JBoss Fuse aside..."
+		echo
+		rm -rf $FUSE_HOME.OLD
+		mv $FUSE_HOME $FUSE_HOME.OLD
+
+		# Unzip the JBoss instance.
+		echo Unpacking JBoss Fuse $VERSION
+		echo
+		unzip -q -d $DEMO_HOME $SRC_DIR/$FUSE_BIN
+else
+		# Unzip the JBoss instance.
+		echo Unpacking new JBoss Fuse...
+		echo
+		unzip -q -d $DEMO_HOME $SRC_DIR/$FUSE_BIN
+fi
+
+echo "  - making sure 'fuse' for server is executable..."
+echo
+chmod u+x $FUSE_HOME/bin/fuse
+
+echo Now going to build the project.
+echo
+cd $PRJ_DIR
+mvn clean install -DskipTests
+
+echo
+echo To get started see the README.md file:
+echo
+cd ../..
+cat README.md
+
+echo Red Hat $DEMO $VERSION Setup Completed.
+echo
+
---
+++ projects/fuse-techtarget/src/main/java/com/mycompany/healthcare/service/PatientLookupService.java
@@ -0,0 +1,17 @@
+package com.mycompany.healthcare.service;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v24.segment.QRD;
+
+public class PatientLookupService {
+    public Message lookupPatient(Message input) throws HL7Exception {
+        QRD qrd = (QRD)input.get("QRD");
+        String patientId = qrd.getWhoSubjectFilter(0).getIDNumber().getValue();
+
+        // find patient data based on the patient id and create a HL7 model object with the response
+        Message response = ;  // create and set response data
+        return response;
+    }
+    
+}
